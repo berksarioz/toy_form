5a9992858d015e5a6cdd1166b18f2f38ae5f5b8d
berksarioz <bsarioz@gmail.com>
2014-03-22 23:25:25 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lostandfound::Application.config.secret_key_base = '9e9dcdd694c98d9627db7ba19e3aa54247f7c6b307cff9c7ed7a242622c8989e55175b237ca01f834314eda53ec4f47559e9937f38a4242ab85e9851df77bb86'
